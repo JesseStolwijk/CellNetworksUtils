300f37747e7e3fa6db0110ffa9d46c49b21cc753
Jesse Stolwijk <stolwijkjesse@gmail.com>
2017-11-30 20:07:01 +0100

    csvByCountry.py will output data foreach mcc into seperate csv files

--- csvByCountry.py
+++ csvByCountry.py
@@ -1,20 +1,39 @@
 import csv
+import os
+from sys import argv
 
 #https://location.services.mozilla.com/downloads
 
-def getstuff(filename):
+print argv
+
+def removeFiles(mydir):
+    if not os.path.exists(mydir):
+        os.makedirs(mydir)
+    else:
+        filelist = [ f for f in os.listdir(mydir) if f.endswith(".csv") ]
+        for f in filelist:
+            os.remove(os.path.join(mydir, f))
+
+def splitCSV(filename):
     with open(filename, "rb") as csvfile:
         reader = csv.reader(csvfile)
         headers = reader.next()
         print(headers)
-        print(','.join(headers))
-        count = 0
-        with open('204.csv', 'w') as the_file:
-            the_file.write(','.join(headers) + '\n')
-            for row in reader:
-                if row[1] == '204':
-                    the_file.write(','.join(row) + '\n')
-                    continue
+
+        vorige = ''
+        for row in reader:
+            if vorige != row[1]:
+                path = 'files/' + row[1] + '.csv'
+                the_file = open(path, 'a')
+                vorige = row[1]
+                if os.stat(path).st_size == 0:
+                    the_file.write(','.join(headers)+ '\n')
+
+            string = ','.join(row)
+            the_file.write(string + '\n')
+                
+
         print("Done")
 
-getstuff("MLSCell.csv")
+removeFiles(argv[2])
+splitCSV(argv[1])
